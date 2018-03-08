e6a5ecdfb0781eafc5b2dd40a3dc93c79b60f895
Pat Breault <pbreault@salsify.com>
2018-03-07 19:40:40 -0500

    allow csv source to take already opened csv table

--- lib/arc-furnace/csv_source.rb
+++ lib/arc-furnace/csv_source.rb
@@ -5,20 +5,23 @@ require 'csv'
 module ArcFurnace
   class CSVSource < EnumeratorSource
     include CSVToHashWithDuplicateHeaders
-    attr_reader :value, :file
+    attr_reader :value, :file, :csv
 
-    def initialize(filename: , encoding: 'UTF-8')
-      @file = File.open(filename, encoding: encoding)
+    def initialize(filename: nil, csv: nil, encoding: 'UTF-8')
+      @file = File.open(filename, encoding: encoding) if filename
+      @csv = csv
       super()
     end
 
     def finalize
-      file.close
+      file.close if file
     end
 
     def build_enumerator
       Enumerator.new do |yielder|
-        CSV.new(file, headers: true).each { |row| yielder << csv_to_hash_with_duplicates(row) }
+        (csv ? csv : CSV.new(file, headers: true)).each do |row|
+          yielder << csv_to_hash_with_duplicates(row)
+        end
       end
     end
   end
