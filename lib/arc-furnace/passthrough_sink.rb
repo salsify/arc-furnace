1dd9f6af5678dc715142598c0523846c2c4f3bca
Coby Durling <cobydurling@Salsify-ITs-MacBook-Pro-11.local>
2018-03-29 17:25:22 -0400

    adding better sink methods

--- lib/arc-furnace/passthrough_sink.rb
+++ lib/arc-furnace/passthrough_sink.rb
@@ -1,11 +1,18 @@
 module ArcFurnace
   class PassthroughSink < Sink
 
+    attr_reader :collection
+
     def initialize(options = {})
+      @collection = Set.new
+    end
+
+    def finalize
+      collection.to_a
     end
 
     def row(object)
-      object
+      collection << object
     end
 
   end
