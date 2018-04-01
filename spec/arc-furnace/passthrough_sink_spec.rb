cc60cc2abe43e1a96a89e6c298e61da8a44c7fe0
Pat Breault <pbreault@salsify.com>
2018-04-01 09:42:21 -0400

    add group_by option to excel source, fix tests

--- spec/arc-furnace/passthrough_sink_spec.rb
+++ spec/arc-furnace/passthrough_sink_spec.rb
@@ -4,9 +4,14 @@ describe ArcFurnace::PassthroughSink do
   let(:sink) { ArcFurnace::PassthroughSink.new }
 
   describe '#row' do
-    it 'works' do
-      expect(sink.row("this")).to eq "this"
-      expect(sink.row({ this: "this" })).to eq({ this: "this" })
+    it 'works with strings' do
+      expect(sink.row('this').to_a).to eq ['this']
+    end
+  end
+
+  describe '#row' do
+    it 'works with hashes' do
+      expect(sink.row({ this: 'this' }).to_a).to eq([{ this: 'this' }])
     end
   end
 
