a751494b83723dcf3c2b3120384fdf2c76a80b71
Pat Breault <pbreault@salsify.com>
2018-10-04 13:40:14 -0700

    add types parameter to excel sink

--- lib/arc-furnace/excel_sink.rb
+++ lib/arc-furnace/excel_sink.rb
@@ -2,14 +2,16 @@ require 'arc-furnace/sink'
 
 module ArcFurnace
   class ExcelSink < Sink
-    private_attr_reader :filename, :fields, :package, :workbook, :worksheet
+    private_attr_reader :filename, :fields, :types, :package,
+      :workbook, :worksheet
 
-    def initialize(filename: , fields:)
+    def initialize(filename: , fields:, types: nil)
       @filename = filename
       @fields = fields
       @package = Axlsx::Package.new
       @workbook = package.workbook
       @worksheet = workbook.add_worksheet(name: 'Sheet1')
+      @types = types ? types : []
       worksheet.add_row(fields)
     end
 
@@ -18,7 +20,7 @@ module ArcFurnace
     end
 
     def row(hash)
-      worksheet.add_row(fields.map { |field_id| hash[field_id] })
+      worksheet.add_row(fields.map { |field_id| hash[field_id] }, types: types)
     end
   end
 end
